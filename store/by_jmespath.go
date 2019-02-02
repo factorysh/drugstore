946aed094e17e5fd6d8451b0367b809a9b846d61
Mathieu Lecarme <mlecarme@bearstech.com>
2019-02-02 20:57:12 +0100

    More jmespath

--- store/by_jmespath.go
+++ store/by_jmespath.go
@@ -5,19 +5,14 @@ import (
 	"strings"
 
 	jmespath "github.com/jmespath/go-jmespath"
+	log "github.com/sirupsen/logrus"
 )
 
 func (s *Store) GetByJMEspath(path string) ([]Document, error) {
-	/*
-		n, err := s.parser.Parse(path)
-		if err != nil {
-			return nil, err
-		}
-		fmt.Println(n)
-	*/
 	p := s.startPath.FindString(path)
 	var docs []Document
 	var err error
+	l := log.WithField("path", path)
 	if p != "" {
 		if strings.HasSuffix(p, ".") {
 			p = p[:len(p)-1]
@@ -25,6 +20,7 @@ func (s *Store) GetByJMEspath(path string) ([]Document, error) {
 		pp := strings.Split(p, ".")
 		docs, err = s.GetByPath(pp...)
 		if err != nil {
+			l.WithError(err).Error("GetByJMEspath")
 			return nil, err
 		}
 	}
@@ -33,18 +29,23 @@ func (s *Store) GetByJMEspath(path string) ([]Document, error) {
 		for _, p := range s.paths {
 			k, ok := d.Data[p]
 			if !ok {
-				return nil, fmt.Errorf("Can't find key %s", p)
+				err := fmt.Errorf("Can't find key %s", p)
+				l.WithError(err).Error("GetByJMEspath")
+				return nil, err
 			}
 			fmt.Println(k)
 			kk, ok := k.(string)
 			if !ok {
-				return nil, fmt.Errorf("Key is not a string : %s => %s", p, k)
+				err := fmt.Errorf("Key is not a string : %s => %s", p, k)
+				l.WithError(err).Error("GetByJMEspath")
+				return nil, err
 			}
 			data[kk] = d
 		}
 	}
 	jm, err := jmespath.Search(path, data)
 	if err != nil {
+		l.WithError(err).Error("GetByJMEspath")
 		return nil, err
 	}
 	fmt.Println(jm)
