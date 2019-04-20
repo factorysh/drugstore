d09048743bb88defd515027ebc867887cbd73896
Mathieu Lecarme <mlecarme@bearstech.com>
2019-04-20 22:08:24 +0200

    More config.

--- conf/conf.go
+++ conf/conf.go
@@ -1,10 +1,13 @@
 package conf
 
 import (
+	"errors"
+
 	"gopkg.in/yaml.v2"
 )
 
 type Conf struct {
+	Listen        string              `yaml:"listen"`
 	PostgresqlDSN string              `yaml:"postgresql_dsn"`
 	Classes       map[string][]string `yaml:"classes"`
 }
@@ -14,6 +17,15 @@ func New(data []byte) (*Conf, error) {
 	err := yaml.Unmarshal(data, &conf)
 	if err != nil {
 		return nil, err
+	}
+	if conf.Listen == "" {
+		conf.Listen = "127.0.0.1:5000"
+	}
+	if conf.PostgresqlDSN == "" {
+		return nil, errors.New("postgresql_dsn is mandatory")
+	}
+	if len(conf.Classes) == 0 {
+		return nil, errors.New("You need at least one class")
 	}
 	return &conf, nil
 }
